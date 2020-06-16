8b0b220afe1cd941d2c01413c29ef9fd6803d8bd
Edd Hannay <edd@vega.xyz>
2020-06-16 19:05:41 +0100

    Adds useless, but less useless tests

--- js/test/outline.test.js
+++ js/test/outline.test.js
@@ -1,7 +1,17 @@
 var vega = require('../generated/proto/api/trading_pb.js')
 var test = require('tape');
 
-test('Passing test', function (t) {
+test('Basic test: Can be imported', function (t) {
   t.plan(1);
-  t.equal(typeof Date.now, 'function');
+  t.equal(typeof vega, 'object');
+});
+
+test('Voting: PrepareVoteRequest exists', function (t) {
+  t.plan(1);
+  t.equal(typeof vega.PrepareVoteRequest, 'function');
+});
+
+test('Voting: PrepareVoteResponse exists', function (t) {
+  t.plan(1);
+  t.equal(typeof vega.PrepareVoteResponse, 'function');
 });
