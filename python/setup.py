20a279722811b4ae96127e77d2da413de5241764
Ashley C <ashley@vega.xyz>
2020-07-25 20:19:07 +0100

    Bump package versions to 0.22.0

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "0.22.0-pre3"
+VERSION = "0.22.0"
 
 setuptools.setup(
     name="Vega API client",
