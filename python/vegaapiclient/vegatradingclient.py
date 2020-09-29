487c81fdb23c750604ba1e866ffc860adee204dc
Ashley C <ashley@vega.xyz>
2020-09-29 16:23:13 +0100

    Add convenience function that prepares, signs and submits a tx

--- python/vegaapiclient/vegatradingclient.py
+++ python/vegaapiclient/vegatradingclient.py
@@ -1,5 +1,9 @@
+import base64
 import grpc
+from typing import Any, Callable
 
+from .walletclient import WalletClient
+from .generated.proto import vega_pb2_grpc as vega_grpc
 from .generated.proto.api import trading_pb2_grpc as trading_grpc
 
 
@@ -19,6 +23,53 @@ class VegaTradingClient(object):
             grpc.channel_ready_future(channel).result(timeout=10)
 
         self._trading = trading_grpc.tradingStub(channel)
+
+    def prepare_sign_submit_tx(
+        self,
+        walletclient: WalletClient,
+        pubkey: str,
+        request: Any,
+        prepare_func: Callable[[Any], Any],
+    ) -> None:
+        """
+        submit_signed_tx is a convenience function that:
+        * prepares the transaction, using the provided function
+        * signs the prepared transaction, using the provided WalletClient
+        * submits the signed transaction.
+        """
+
+        # Prepare the transaction
+        response = prepare_func(request)
+        blob_base64 = base64.b64encode(response.blob).decode("ascii")
+
+        # Sign the transaction
+        response = walletclient.signtx(blob_base64, pubkey)
+        if response.status_code != 200:
+            raise Exception(
+                "Error: Wallet failed to sign transaction: HTTP {} {}".format(
+                    response.status_code,
+                    response.text,
+                )
+            )
+
+        signedTx = response.json()["signedTx"]
+
+        # Submit the signed transaction
+        request = trading_grpc.SubmitTransactionRequest(
+            tx=vega_grpc.SignedBundle(
+                tx=base64.b64decode(signedTx["tx"]),
+                sig=vega_grpc.Signature(
+                    sig=base64.b64decode(signedTx["sig"]["sig"]),
+                    algo="vega/ed25519",
+                    version=1,
+                ),
+            ),
+        )
+        response = self.SubmitTransaction(request)
+        if not response.success:
+            raise Exception(
+                "Vega node failed to submit signed transaction (success=false)"
+            )
 
     def __getattr__(self, funcname):
         return getattr(self._trading, funcname)
