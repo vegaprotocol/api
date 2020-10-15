4250e8e8e65aedf20c0d415f6c88774809205ca3
Ashley C <ashley@vega.xyz>
2020-10-15 09:20:55 +0100

    Set version 0.25.0

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "0.26.0-pre1"
+VERSION = "0.25.1"
 
 setuptools.setup(
     name="Vega API client",
