916e6e8e0efb0f662c614ba51d000fc9fc1bc2e8
Ashley C <ashley@vega.xyz>
2020-11-03 13:14:10 +0000

    Set version 0.27.0-pre3

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Vega API auto-generated gRPC clients
 
-Version: 0.27.0-pre1
+Version: 0.27.0-pre3
 
 This repository contains auto-generated gRPC clients for various languages.
 
--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "0.27.0-pre1"
+VERSION = "0.27.0-pre3"
 
 setuptools.setup(
     name="Vega API client",
