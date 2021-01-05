680583e14efa70bbebf67615836590dc5283160b
Edd Hannay <edd@vega.xyz>
2021-01-05 18:48:05 +0000

    Add a static index.d.ts and a reference in package
    To make dependent packages recognise the types, we need to add
    a declaration that there are indeed types to look for. This commit
    adds an `index.d.ts` file in the root that contains references to the
    .d.ts files generated in a previous commit in this branch. It also adds
    a reference to that file in `package.json`, which is The Correct Way,
    even though `index.d.ts` should be sufficient.
    
    However! This requires a follow up commit that generates the
    `index.d.ts` file in the build script, rather than leaving it hardcoded.
    The current file is good for now, but should be removed and generated at
    build.
    
    - Add `types` field in `package.json`
    - Add hardcoded `index.d.ts`

---
+++ js/index.d.ts
@@ -0,0 +1,8 @@
+export * as assets from "./generated/proto/assets_pb.d.ts";
+export * as chain_events from "./generated/proto/chain_events_pb.d.ts";
+export * as events from "./generated/proto/events_pb.d.ts";
+export * as governance from "./generated/proto/governance_pb.d.ts";
+export * as markets from "./generated/proto/markets_pb.d.ts";
+export * as vega from "./generated/proto/vega_pb.d.ts";
+export * as api_trading from "./generated/proto/api_trading_pb.d.ts";
+export * as tm_replay from "./generated/proto/tm_replay_pb.d.ts";
