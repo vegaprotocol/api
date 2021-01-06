a66049613440080475b1e26c5636a8961d6c4958
Edd Hannay <edd@vega.xyz>
2021-01-06 13:20:52 +0000

    Add serialisation tests and validation

--- js/test/vega/order.test.js
+++ js/test/vega/order.test.js
@@ -5,7 +5,7 @@ var { skipIfLocalOnly } = require('../lib/env')
 // var protoLoader = require('@grpc/proto-loader');
 var xhr = require("xmlhttprequest");
 
-var x = require('../../index')
+var { vega } = require('../../index')
 
 function wallet_server_login(walletServerURL, walletName, walletPassphrase) {
     var request = new xhr.XMLHttpRequest();
@@ -23,15 +23,99 @@ function wallet_server_login(walletServerURL, walletName, walletPassphrase) {
             throw "Failed to get token from response body: " + request.responseText;
         }
     }
-    request.open("POST", wserver + "/api/v1/auth/token", true);
+    request.open("POST", walletServerURL + "/api/v1/auth/token", true);
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    request.send(JSON.stringify({wallet: wname, passphrase: wpass}, null, 2));
+    request.send(JSON.stringify({wallet: walletName, passphrase: walletPassphrase}, null, 2));
     return token;
 }
 
 test('Basic test: Can create an order', t => {
-    const order = new x.vega.OrderSubmission()
+    const order = new vega.OrderSubmission()
     t.equal(typeof order, 'object')
+    t.end()
+})
+
+test('Basic test: Can serialise an empty order to an unsigned tx', t => {
+    const order = new vega.OrderSubmission()
+    t.deepEqual(order.serializeBinary(), new Uint8Array(), 'An empty order should serialise to an empty Uint8Array')
+    t.end()
+})
+
+test('A basic order can be constructed and serialised to an unsigned tx', t => {
+    const hardcodedExpectedOutput =  new Uint8Array([18, 14, 116, 101, 115, 116, 45, 109, 97, 114, 107, 101, 116, 45, 105, 100, 32, 10, 40, 20, 48, 1])
+
+    const order = new vega.OrderSubmission()
+    order.setMarketid('test-market-id')
+    order.setPrice(10)
+    order.setSize(20)
+    order.setSide(vega.Side.SIDE_BUY)
+
+    t.deepEqual(order.serializeBinary(), hardcodedExpectedOutput, 'The serialised order should match the hardcoded output')
+    t.end()
+})
+
+test('A basic order can be deserialised', t => {
+    const existingSerializedOrder =  new Uint8Array([18, 14, 116, 101, 115, 116, 45, 109, 97, 114, 107, 101, 116, 45, 105, 100, 32, 10, 40, 20, 48, 1])
+
+    const order = new vega.OrderSubmission.deserializeBinary(existingSerializedOrder)
+
+    t.equal(order.getMarketid(), 'test-market-id')
+    t.equal(order.getPrice(), 10)
+    t.equal(order.getSize(), 20)
+    t.equal(order.getSide(), vega.Side.SIDE_BUY)
+
+    t.end()
+})
+
+test('Order Validation: An order size must be a number', t => {
+    const order = new vega.OrderSubmission()
+    order.setSize('this is bad')
+
+    t.equal(order.getSize(), 'this is bad')
+    t.throws(() => {
+       order.serializeBinary()
+    },'Assertion failed', 'An order with a size set to a string should fail to serialise')
+    t.end()
+})
+
+test('Order Validation: An order price must be a number', t => {
+    const order = new vega.OrderSubmission()
+    order.setPrice('this is bad')
+
+    t.equal(order.getPrice(), 'this is bad')
+    t.throws(() => {
+        order.serializeBinary()
+    },'Assertion failed', 'An order with a price set to a string should fail to serialise')
+    t.end()
+})
+
+test('Order Validation: An order expiry date must be a number', t => {
+    const dateShouldBeANumber = new Date().toDateString()
+
+    const order = new vega.OrderSubmission()
+
+    order.setExpiresat(dateShouldBeANumber)
+
+    t.equal(order.getExpiresat(), dateShouldBeANumber)
+    t.throws(() => {
+        order.serializeBinary()
+    },'Assertion failed', 'An order with a date set to a string should fail to serialise')
+    t.end()
+})
+
+test('Order can be a pegged order when an order peg is provided', t => {
+    const dateShouldBeANumber = new Date().toDateString()
+
+    const order = new vega.OrderSubmission()
+    const orderPeg = new vega.PeggedOrder()
+    orderPeg.setOffset(1)
+
+    order.setPeggedorder(orderPeg)
+    t.deepEqual(order.getPeggedorder(), orderPeg)
+
+    t.doesNotThrow(() => {
+        order.serializeBinary()
+    })
     t.end()
 })
 
