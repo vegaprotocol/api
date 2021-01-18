fa64e210e51ee4e3a9c28fd26d6ab2712dcd4592
Edd Hannay <edd@vega.xyz>
2021-01-18 14:44:49 +0000

    Remove extra extension

--- js/index.d.ts
+++ js/index.d.ts
@@ -6,7 +6,7 @@ export * as events from "./generated/events_pb";
 export * as governance from "./generated/governance_pb";
 export * as markets from "./generated/markets_pb";
 export * as vega from "./generated/vega_pb";
-export * as tx from "./tx/index.ts";
+export * as tx from "./tx/index";
 export * as api_trading from "./generated/api/trading_pb";
 export * as go_proto_validators_validator from "./generated/github.com/mwitkow/go-proto-validators/validator_pb";
 export * as tm_replay from "./generated/tm/replay_pb";
