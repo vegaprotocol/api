830ec150220d0b41e5b5197aecc1f25fc9b90df1
Edd Hannay <edd@vega.xyz>
2021-01-18 13:11:12 +0000

    Add tx to .d.ts export

--- js/.generate_indexjs.sh
+++ js/.generate_indexjs.sh
@@ -20,6 +20,7 @@ done
 # Also include known JS exports
 echo "var tx = require('./tx/index')" >>"$requires"
 echo "  tx: tx," >>"$modexports"
+echo "export * as tx from \"./tx/index.ts\";" >>"$ts_exports"
 
 # Handle proto subdirs
 find "$protodir" -name '*_pb.js' -print0 | xargs -0 dirname | sort -u | tail +2 | while read -r protosubdir ; do
