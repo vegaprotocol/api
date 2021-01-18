f196fc55ccf3f7315da5d4ed808347e24346ebb7
Matthew Russell <mattrussell36@gmail.com>
2021-01-18 13:02:42 -0700

    ensure we test the compiled decodeTx commonjs module rather than the ts file

--- js/test/decode.test.js
+++ js/test/decode.test.js
@@ -1,11 +1,10 @@
-
 const test = require('tape');
-const tx = require('../index').tx.default;
+const { decodeTx } = require('../tx/decode.js')
 
 const encodedTx = `CvABCsABNDliOTk4MzEtMmRjYS00YzA1LThhZTAtN2I3MDA4YWYzN2M4QBIQMjgzOUQ5QjIzMjlDOUU3MBpAMGZjOTM2YWJjNTFkYzk0OTkxOWVkOGE1NmM5MzRkZmFiMWMxNTFkNWFmMWFiMzZjZTc2NDdjNmY1YTYzMTc4OCgiMAE4AkDE5rTVmfDXrRZIAVIkYTg1ZDVlYTctZjNmNi00NjE1LTkyNDUtNzM0NjA3NDBkODM5Wg0IAhD///////////8BEMi/pITL8J2wQ9I+IA/JNqvFHclJkZ7YpWyTTfqxwVHVrxqzbOdkfG9aYxeIElIKQL7B8BUvF+z/fdRi4thGvvpzFuzXdZUov3V2Xt+k0Q992B2JKChioGQ7NpG1mFriYrnwj8n7gCa3kPcX35nqbAcSDHZlZ2EvZWQyNTUxORgB`
 
 test('A raw encoded TX decodes', t => {
-   const res = tx.decodeTx(encodedTx)
+   const res = decodeTx(encodedTx)
 
    t.equal(res.marketid, '2839D9B2329C9E70')
    t.end()
@@ -13,7 +12,7 @@ test('A raw encoded TX decodes', t => {
 
 test('A totally invalid transaction throws an error', t => {
    t.throws(() => {
-       tx.decodeTx('decode-this')
+       decodeTx('decode-this')
    }, /Cannot decode signed bundle/)
 
    t.end()
@@ -21,7 +20,7 @@ test('A totally invalid transaction throws an error', t => {
 
 test('A more reasonable invalid signed transaction throws an error', t => {
    t.throws(() => {
-       tx.decodeTx(encodedTx.substring(20))
+       decodeTx(encodedTx.substring(20))
    }, /Cannot decode signed bundle/)
 
    t.end()
