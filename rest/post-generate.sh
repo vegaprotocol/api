81ccca1e0fe9deac19b6793acdae653cc76e58a2
Ashley C <ashley@vega.xyz>
2021-03-29 17:22:40 +0100

    Replace merge.py with jq

--- rest/post-generate.sh
+++ rest/post-generate.sh
@@ -5,8 +5,5 @@ set -e
 # Inject extra content into spec
 t="$(mktemp -p "$PWD" -t tmp-XXXXXXXX.json)"
 final=rest/api/trading.swagger.json
-python3 .merge.py \
-	"$final" \
-	rest/extra.json \
-	>"$t"
+jq -sMS '.[0] * .[1]' "$final" rest/extra.json >"$t"
 mv "$t" "$final"
