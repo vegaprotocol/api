40c33e34a7b7bae500e620479555e8e461393c88
cdm <chris.moores@gmail.com>
2021-05-18 16:44:42 +0100

    Add --ci support logic to login.py etc

--- rest/examples/python/helpers.py
+++ rest/examples/python/helpers.py
@@ -1,38 +1,80 @@
 import random
+import os
 import requests
 import string
 from typing import Any
 
 
 def check_response(r: requests.Response) -> None:
-    assert (
-        r.status_code == 200
-    ), f"{r.url} returned HTTP {r.status_code} {r.text}"
+    """
+    Raise a helpful exception if the HTTP response was not 200.
+    """
+    if r.status_code != 200:
+        raise Exception(f"{r.url} returned HTTP {r.status_code} {r.text}")
 
 
 def check_var(val: str) -> bool:
-    return val is not None and val != "" and "example" not in val
-
+    """
+    Return true if the value is ok.
+    """
+    if val is None or val == "":
+        return False
+    if invalid(val):
+        return False
+    return True
 
 def check_url(url: str) -> bool:
-    return check_var(url) and (
-        url.startswith("https://") or url.startswith("http://")
-    )
+    """
+    Return true if the URL is ok.
+    """
+    if not check_var(url):
+        return False
+    if not any(url.startswith(prefix) for prefix in ["http://", "https://"]):
+        return False
+    return True
+
+
+def get_from_env(var_name: str) -> str:
+    """
+    Get a value from an environment variable. Used in CI for testing.
+    """
+    val = os.getenv(var_name, "")
+    if val == "":
+        print(f"Error: Missing environment variable {var_name}.")
+        exit(1)
+
+    return val
+
+
+def invalid(val: str) -> bool:
+    """
+    Return true if none of the invalid strings are found in the value.
+    """
+    bzzt = [">>", "e.g.", "example"]
+    return any(x in val for x in bzzt)
 
 
 def random_string(length: int = 20) -> str:
+    """
+    Generate a random string, made of letters and digits.
+    """
     choices = string.ascii_letters + string.digits
     return "".join(random.choice(choices) for i in range(length))
 
 
-def check_wallet_url(url: str) -> str:
-    for suffix in ["/api/v1/", "/api/v1", "/"]:
-        if url.endswith(suffix):
-            print(
-                f'There\'s no need to add "{suffix}" to WALLETSERVER_URL. '
-                "Removing it."
-            )
-            url = url[: -len(suffix)]
+def fix_walletserver_url(url: str) -> str:
+    """
+    Help guide users against including api version suffix in wallet server URL.
+    """
+    for suffix in ["/api/v1/", "/api/v1", "/api/", "/api", "/"]:
+        if not url.endswith(suffix):
+            continue
+        print(
+            f'There\'s no need to add "{suffix}" to the wallet server URL. '
+            "Removing it and continuing..."
+        )
+        url = url[: -len(suffix)]
+
     return url
 
 
--- rest/examples/python/login.py
+++ rest/examples/python/login.py
@@ -3,17 +3,19 @@
 import requests
 import os.path
 import json
+import sys
 import helpers
 
-wallet_server_url = os.getenv("WALLETSERVER_URL")
-if not helpers.check_url(wallet_server_url):
-    print("error: Missing wallet server URL, please set this in configuration")
-    exit(1)
+ci_args = "--ci"
+wallet_server_url = helpers.get_from_env("WALLETSERVER_URL")
 
 
 def load_token():
+    if ci_args in sys.argv:
+        return perform_login()
+        # login will call exit with appropriate code
     if not os.path.isfile("token.temp"):
-        print('error: no token file found: try running "python3 login.py"')
+        print('Error: No token file found: try running "python3 login.py"')
         exit(1)
     with open("token.temp", "r") as token_file:
         return json.load(token_file)["token"]
@@ -23,33 +25,44 @@ def get_pubkey(token):
     auth_headers = {"Authorization": "Bearer " + token}
     response = requests.get(wallet_server_url + "/api/v1/keys", headers=auth_headers)
     if response.status_code != 200:
-        print("error listing keys: " + response.text)
+        print("Error listing keys: " + response.text)
         exit(1)
     keys = response.json()["keys"]
     if len(keys) < 1:
-        print("error: no keys found, use console or Vega Wallet CLI to create one")
+        print("Error: No keys found, use Vega Console or Vega Wallet CLI to create one")
         exit(1)
     return keys[0]["pub"], auth_headers
 
 
-if __name__ == "__main__":
+def perform_login():
     import getpass
-
     wallet_name = os.getenv("WALLET_NAME")
     wallet_passphrase = os.getenv("WALLET_PASSPHRASE")
-
-    if not helpers.check_var(wallet_name) or not helpers.check_var(wallet_passphrase):
+    valid_user = helpers.check_var(wallet_name)
+    valid_pass = helpers.check_var(wallet_passphrase)
+    if ci_args in sys.argv and not valid_user:
+        print(f"Error: Missing environment variable WALLET_NAME")
+        exit(1)
+    if ci_args in sys.argv and not valid_pass:
+        print(f"Error: Missing environment variable WALLET_PASSPHRASE")
+        exit(1)
+    if not valid_user or not valid_pass:
         wallet_name = input("Enter Vega wallet username: ")
         wallet_passphrase = getpass.getpass("Vega wallet passphrase: ")
-
     req = {"wallet": wallet_name, "passphrase": wallet_passphrase}
     response = requests.post(wallet_server_url + "/api/v1/auth/token", json=req)
     if response.status_code != 200:
-        print("error logging in: " + response.text)
+        print("Error logging in: " + response.text)
         exit(1)
-    with open("token.temp", "w") as token_file:
-        json.dump(response.json(), token_file)
-    print('Token saved to "token.temp", done.')
+    if ci_args not in sys.argv:
+        with open("token.temp", "w") as token_file:
+            json.dump(response.json(), token_file)
+        print('Token saved to "token.temp", done.')
+    return response.json()["token"]
+
+
+if __name__ == "__main__":
+    perform_login()
     exit(0)
 
 
