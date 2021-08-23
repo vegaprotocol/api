5156bb3a5af7b164c171c2050da7b709a09c1a31
Ashley C <ashley@vega.xyz>
2021-08-23 14:57:51 +0100

    JS: (re)set version 0.41.0; fix package files

--- grpc/clients/python/setup.py
+++ grpc/clients/python/setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "0.41.0-pre4"
+VERSION = "0.41.0"
 
 setuptools.setup(
     name="Vega API client",
