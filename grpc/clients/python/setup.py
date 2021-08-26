504d2f235ee5fbfd5a55593679601fd6ac81163d
Ashley C <ashley@vega.xyz>
2021-08-26 17:31:54 +0100

    FUDGE Set version 0.42.0-pre2

--- grpc/clients/python/setup.py
+++ grpc/clients/python/setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "0.42.0-pre1"
+VERSION = "0.42.0-pre2"
 
 setuptools.setup(
     name="Vega API client",
